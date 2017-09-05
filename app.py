4a28d9a5022bd0cf8be06530359e46a9cc1f9da0
ram-d <devi.thulasiram@gmail.com>
2017-09-05 17:00:25 -0400

    d

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def makeWebhookResult(req):
         parameters = result.get("parameters")
         person = parameters.get("person")
         #contexts = result.get("contexts")
-        amount = result.get("contexts")[0].get("unit-currency.original")
+        amount = result.get("contexts")[1].get("unit-currency.original")
         print(amount)
         speech = "Sure, I have successfully transferred " + amount + " to your " + person + " from your Checking account. Would you like to perform any other transaction?"
         print("Response:")
