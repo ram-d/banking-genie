0cad64a55847131a34c2fe9046fdf34a28d26341
ram-d <devi.thulasiram@gmail.com>
2017-09-05 15:24:04 -0400

    d

--- app.py
+++ app.py
@@ -31,9 +31,8 @@ def makeWebhookResult(req):
     if req.get("result").get("action") != "account.balance":
         return {}
     result = req.get("result")
-    contexts = result.get("contexts")
-    parameters = contexts.get("parameters")
-    accountType = parameters.get("account-type.original")
+    parameters = result.get("parameters")
+    accountType = parameters.get("account-type")
     balance = {'Checking':4500, 'Savings':200, 'Business Checking':15000, 'Business Savings':1500}
     speech = "The balance on your " + accountType + " Account is " + str(balance[accountType]) + " dollars. Is there anything else I can help you with?"
     print("Response:")
